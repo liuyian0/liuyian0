b442b530d682468a82d7167bd8fc1f191740f2ca
Liuyian <1203733192@qq.com>
2023-12-18 08:21:59 +0000

    最大值

---
+++ src/WhoBig.java
@@ -0,0 +1,12 @@
+public class WhoBig {
+public static void main(String[] args) {
+  int arr[] = {1,2,9,90,89,68,55,766};
+  int max = arr[0];
+  for (int i = 1; i <= arr.length-1; i++) {
+      if(arr[i]>max){
+          max = arr[i];
+      }
+  }
+        System.out.println("最大值为"+max);
+}
+}
