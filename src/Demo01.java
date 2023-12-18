c562ecfec8466ebeb33fde45fef8cd493622eb13
Liuyian <1203733192@qq.com>
2023-12-18 07:41:07 +0000

    测试提交

---
+++ src/Demo01.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Demo01 {
+public static void main(String[] args) {
+  Random r = new Random();
+  int num = r.nextInt(100)+1;
+  System.out.println(num);
+  Scanner s = new Scanner(System.in);
+  while (true) {
+      System.out.println("请输入一个数字:");
+      int guessNum = s.nextInt();
+      if (guessNum > num) {
+          System.out.println("猜大了");
+      }else if (guessNum < num) {
+          System.out.println("猜小了");
+      }else{
+          System.out.println("猜对了");
+          break;
+      }
+  }
+}
+}
