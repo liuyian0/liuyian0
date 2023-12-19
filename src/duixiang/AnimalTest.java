20f982b3f97d4abbcaf69b5fd9102ae0d2a8ca72
Liuyian <1203733192@qq.com>
2023-12-19 09:31:23 +0000

    封装（this关键字的使用）

---
+++ src/duixiang/AnimalTest.java
@@ -0,0 +1,10 @@
+package duixiang;
+
+public class AnimalTest {
+public static void main(String[] args) {
+  Animal cat = new Animal();
+  cat.setName("猫");
+  cat.setAge(3);
+  System.out.println(cat.getName()+cat.getAge());
+}
+}
