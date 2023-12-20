35d195097905d07d911c6d3e29e7597679197f76
Liuyian <1203733192@qq.com>
2023-12-20 06:46:57 +0000

    新建文件夹

---
+++ src/com/liu/day01/duixiang/Pupil.java
@@ -0,0 +1,18 @@
+package duixiang;
+
+ public class Pupil {
+     private String name;
+     private int age;
+     //无参构造
+     public Pupil(){}
+     //有参构造
+     public Pupil (String name,int age){
+         this.name = name;
+             this.age = age;
+             }
+             public void show(){
+                 System.out.println(name+","+age);
+                 }
+                 }
+                 
+ }
