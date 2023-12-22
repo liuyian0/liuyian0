f814836994c76492e79cbef4b8c42569049dfafd
Liuyian <1203733192@qq.com>
2023-12-22 09:30:52 +0000

    匿名对象的使用和弊端

--- src/com/liu/day01/duixiang/StudentTest.java
+++ src/com/liu/day01/duixiang/StudentTest.java
@@ -7,5 +7,10 @@ public static void main(String[] args) {
   s.name = "张三";
   s.setAge(18);
   System.out.println(s.name+s.getAge());
+  /*匿名对象:创建对象时只有创建对象的语句，却没有把对象的地址赋值给某个变量
+    如果对象需要多次操作成员，就不能使用匿名对象，否则会出错
+  */
+  new Student().name = "赵四";
+  new Student().setAge(20);
 }
 }
