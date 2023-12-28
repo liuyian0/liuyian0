693c66e713fb58132daf6efb709d55cae7daf9f9
Liuyian <1203733192@qq.com>
2023-12-28 09:16:35 +0000

    静态函数，也称类函数

--- src/com/liu/day01/StaticTest/PersonTest.java
+++ src/com/liu/day01/StaticTest/PersonTest.java
@@ -3,6 +3,7 @@ package com.liu.day01.StaticTest;
 public class PersonTest {
     public static void main(String[] args) {
       /*
+      静态函数，也称类函数
       被static修饰的函数，不需要对象调用，可以直接类名.函数名方式直接调用。
       public static void sleep(){
         System.out.println("睡觉");
