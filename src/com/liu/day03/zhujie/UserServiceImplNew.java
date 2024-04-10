796a3df441761bfd4c7e17d84c5c0df8e89f6b24
liu <1203733192@qq.com>
2024-04-10 15:48:42 +0800

    dtd约束和schema约束

--- src/com/liu/day03/zhujie/UserServiceImplNew.java
+++ src/com/liu/day03/zhujie/UserServiceImplNew.java
@@ -1,7 +1,5 @@
 package com.liu.day03.zhujie;
 
-import com.liu.day03.ClassLoader.Student;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
